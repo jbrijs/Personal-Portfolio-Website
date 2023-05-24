714e1663729dab61784bdd46da1efaebb31c237b
Joe Brijs <joebrijs@gmail.com>
2023-05-23 18:33:44 -0600

    finished navbar transtion to hidden

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", ()=> {
     if (lastScrollY < window.scrollY){
-        console.log("we are going down");
         navbar.dataset.hidden = "true";
     }
     else{
-        console.log("we are going up");
         navbar.dataset.hidden = "false";
     }
 
