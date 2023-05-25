df6f9944bbb03c43e4f7d15fec727ccb10c93ea9
Joe Brijs <joebrijs@gmail.com>
2023-05-24 22:18:30 -0600

    fixed home page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 
-
+// Navbar is hidden when scrolling down, visible when scrolling up
 const navbar = document.querySelector('.navbar');
 let lastScrollY = window.scrollY;
-
 
 window.addEventListener("scroll", ()=> {
     if (lastScrollY < window.scrollY){
         navbar.dataset.hidden = "true";
-    }
-    else{
+    } else{
         navbar.dataset.hidden = "false";
     }
 
     lastScrollY = window.scrollY;
 })
+
