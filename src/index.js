9a12bae1ad70f3320cef85a15fbe334270880b44
Joe Brijs <joebrijs@gmail.com>
2023-05-25 22:46:33 -0600

    added more to the event listeners of the courses, changed font to inter from railway

--- src/index.js
+++ src/index.js
@@ -16,7 +16,16 @@ const numCourses = 5;
 
 for (i=0; i < numCourses; i++){
     let course = document.getElementById("course-" + i);
-    course.addEventListener("click", ()=>{
-        course.dataset.hidden = "true";
+    course.addEventListener("click", (e)=>{
+        let id = course.id
+        for (i=0; i < numCourses;i++){
+            let checkElement = document.getElementById("course-" + i);
+            if (id != checkElement.id){
+                checkElement.dataset.hidden = "true";
+            
+            }
+        }
+        
+        // course.dataset.hidden = "true";
     })
 }
