22d3f0f5279dcdebb8894fc1e72453587570a6b9
jbrijs <joebrijs@gmail.com>
2024-03-07 18:39:57 -0700

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      background: '#111A2C'
+    },
+  },
+  plugins: [],
+}
+
